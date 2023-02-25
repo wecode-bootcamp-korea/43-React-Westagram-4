8a4f79c17f9dd5782caba460985e145673353771
HongTaeHoon <kdn95374888@gmail.com>
2023-02-25 16:27:28 +0900

    "Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현" "Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현(삼항연산자 사용용X)"

--- src/pages/taehoon/Login/LoginTaehoon.js
+++ src/pages/taehoon/Login/LoginTaehoon.js
@@ -1,7 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginTaehoon.scss';
+import { useNavigate } from 'react-router-dom';
+
 const LoginTaehoon = () => {
-  return <h1>Taehoon Hello</h1>;
+  const navigte = useNavigate();
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
+  const [changeButtonColor, setChangeButtonColor] =
+    useState('changeButtonColor');
+
+  const buttonColor = () => {
+    setChangeButtonColor('blue');
+  };
+
+  const onKeyUp = event => {
+    if (id.includes('@') && pw.length >= 5) {
+      buttonColor();
+      if (event.key === 'Enter') {
+        goToMain();
+      }
+    } else {
+      if (event.key === 'Enter') {
+        alert('아이디를 확인해주세요.');
+      }
+    }
+  };
+  const onClick = () => {
+    if (id.includes('@') && pw.length >= 5) {
+      goToMain();
+    } else {
+      alert('아이디를 확인해주세요.');
+    }
+  };
+
+  const saveUserId = e => {
+    setId(e.target.value);
+  };
+  const saveUserPw = e => {
+    setPw(e.target.value);
+  };
+  const goToMain = () => {
+    navigte('/main-taehoon');
+  };
+
+  return (
+    <div onKeyUp={onKeyUp} className="login">
+      <div className="loginPage">
+        <div className="westagramBox">westagram</div>
+        <div className="idBox">
+          <input
+            type="text"
+            id="id"
+            value={id}
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+            onChange={saveUserId}
+          />
+        </div>
+
+        <div className="passwordBox">
+          <input
+            type="password"
+            id="password"
+            value={pw}
+            placeholder="비밀번호"
+            onChange={saveUserPw}
+          />
+        </div>
+
+        <button className={changeButtonColor} value="value" onClick={onClick}>
+          로그인
+        </button>
+
+        <div className="forgetPassword">비밀번호를 잊으셨나요?</div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginTaehoon;
--- src/pages/taehoon/Main/MainTaehoon.js
+++ src/pages/taehoon/Main/MainTaehoon.js
@@ -1,7 +1,99 @@
 import React from 'react';
 import './MainTaehoon.scss';
+import instargramIcon from '../../../assets/taehoon/instagram.png';
+import mari from '../../../assets/taehoon/IMG_1211.JPG';
+import feedImg from '../../../assets/taehoon/IMG_1278.jpeg';
 const MainTaehoon = () => {
-  return <h1>Taehoon Main</h1>;
+  return (
+    <div className="main">
+      <div className="bar">
+        <nav className="navigation">
+          <div className="navLeft">
+            <img className="instargramIcon" src={instargramIcon} alt="none" />
+            <div className="navTitle">westagram</div>
+          </div>
+          <input type="text" placeholder="검색" className="searchBox" />
+          <div>
+            <img
+              className="navIcon"
+              src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/explore.png"
+              alt=""
+            />
+            <img
+              className="navIcon"
+              src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
+              alt=""
+            />
+            <img
+              className="navIcon"
+              src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/profile.png"
+              alt=""
+            />
+          </div>
+        </nav>
+        <main className="mainPage">
+          <div className="mainfeed">
+            <section className="mainLeft">
+              <div className="feed">
+                <artcle>
+                  <div className="writer">
+                    <img src={mari} alt="" className="mari" />
+                    <h5 className="name">taehoon_hong</h5>
+                  </div>
+                  <img className="myPhoto" src={feedImg} alt="" />
+                  <div>
+                    <section>
+                      <i id="awsomeIcon" className="fa-regular fa-heart" />
+                      <i id="awsomeIcon" className="fa-regular fa-message" />
+                      <i
+                        id="awsomeIcon"
+                        className="fa-solid fa-arrow-up-from-bracket"
+                      />
+                    </section>
+                    <section>
+                      <span className="whoIsLike">여러명이 좋아합니다.</span>
+
+                      <div className="commentList" />
+
+                      <div className="addComment">
+                        <input
+                          type="text"
+                          id="comment"
+                          placeholder="댓글 달기..."
+                        />
+                        <button id="uploadComment">게시</button>
+                      </div>
+                    </section>
+                  </div>
+                </artcle>
+              </div>
+            </section>
+            <section className="mainRight">
+              <div className="writer">
+                <img src={mari} alt="" className="idOwner" />
+                <h6 className="idOwnerName">taehoon_hong</h6>
+              </div>
+              <div className="story">
+                <h6 className="storyTitle">스토리</h6>
+                <div className="storyList">
+                  <img src={mari} alt="" className="storyImage" />
+                  <h6 className="storyOwner">Hong</h6>
+                </div>
+                <div className="storyList">
+                  <img className="storyImage" src={mari} alt="" />
+                  <h6 className="storyOwner">HongTaeHoon</h6>
+                </div>
+                <div className="storyList">
+                  <img className="storyImage" src={mari} alt="" />
+                  <h6 className="storyOwner">TaeHoon</h6>
+                </div>
+              </div>
+            </section>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export default MainTaehoon;
